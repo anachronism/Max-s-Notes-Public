d599f41f1b1ab087302a19d664796944cba4274e
Li <mhli@wpi.edu>
2017-11-03 10:37:43 -0400

    got pca working correctly, eigenface looks like a face

--- Mach_vis_code/assignment_3.m
+++ Mach_vis_code/assignment_3.m
@@ -1,6 +1,19 @@
 clearvars;
 close all;
+
+
 faceDir = 'faces';
+dim = 30;
+
 [images,personID,lightingCond,subset] = readFaceImages(faceDir);
-x = pca_50x50(images);
-y = 1;
+[x_norm,pcaSpace,lambda] = pca_50x50(images,dim);
+
+
+
+% Look at eigenfaces generated.
+x_mean = mean(x_norm,1);
+testImg = x_mean + pcaSpace(:,1).';
+testImg = reshape(testImg,[50,50]);
+
+figure();
+imshow(testImg);
